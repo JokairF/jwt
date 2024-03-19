aac8d721382886b58dd52c37d736f087b853ec0e
Erik <erik.blacioriofrio@gmail.com>
2024-03-19 10:55:38 +0100

    add require vendor/autoload.php

--- secure_page.php
+++ secure_page.php
@@ -1,11 +1,15 @@
 <?php
-    session_start();
-    
-    if( !isset($_SESSION["username"]) || $_SESSION["username"]==""){
-        header("Location: index.php?error=1");
-    }
 
-    echo "<h1>Bienvenue, " . $_SESSION["username"] . "!</h1>";
-    echo "<p>Vous êtes connecté à la page sécurisée.</p>";
+session_start();
 
-?>
+require __DIR__ . "vendor/autoload.php";
+
+use Firebase\JWT;
+use Firebase\Key;
+
+if (!isset($_SESSION["username"]) || $_SESSION["username"] == "") {
+    header("Location: index.php?error=1");
+}
+
+echo "<h1>Bienvenue, " . $_SESSION["username"] . "!</h1>";
+echo "<p>Vous êtes connecté à la page sécurisée.</p>";
